91f68482e1c20b8bf29ddedc47be8b55920e16ff
gabrieldubowski <gabrieldubowski@Icloud.com>
2026-05-28 21:39:43 -0300

    Meu projeto mobile

---
+++ Meu-Projeto-Mobile/dados.ts
@@ -0,0 +1,12 @@
+export type Evento = {
+  id: string;
+  nome: string;
+  descricao: string;
+  categoria: string;
+  tagColor?: string;
+  data: string;
+  local: string;
+  ingresso: string;
+  latitude: string;
+  longitude: string;
+};
